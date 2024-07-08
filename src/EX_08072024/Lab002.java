d65f1a96e9aacaa564c7050efb8c43f36bf703b7
RAHUL26072001 <rsnegi1002@gmail.com>
2024-07-08 12:22:22 +0530

    All Files are added

---
+++ src/EX_08072024/Lab002.java
@@ -0,0 +1,9 @@
+package EX_08072024;public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(3/3);
+
+    }
+}
